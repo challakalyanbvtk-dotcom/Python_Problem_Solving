22bb61d51b636273c5b056f883d4f6f31aa4017b
Bhanu Venkata Thiru Kalyan Challa <challakalyanbvtk@gmail.com>
2026-06-15 00:46:17 +0530

    Delete ID_Stripper.py

--- ID_Stripper.py
+++
@@ -1,9 +0,0 @@
-import os
-import glob
-for filepath in glob.glob("*.py"):
-    with open(filepath, 'r', encoding='utf-8') as file:
-        lines = file.readlines()
-    clean_lines = [line for line in lines if not line.strip().startswith('#') and line.strip() != '']
-    with open(filepath, 'w', encoding='utf-8') as file:
-        file.writelines(clean_lines)
-print("All author comments and personal headers have been stripped!")
